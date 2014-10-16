7c15898f94849a5cc1e91a5c6eea97b2ade9de7a
Fabrice Weinberg <Fabrice@weinberg.me>
2014-10-16 19:43:12 +0200

    Cleanup

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-http = require("http");
+var http = require("http");
+var needle = require("needle");
+var moment = require('moment');
+var url = require('url');
 
-server = http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
 
-	loginCookies = [];
-	var query = require('url').parse(req.url, true).query;
-	needle = require("needle");
+  var loginCookies = [];
+  var query = url.parse(req.url, true).query;
 
-	// Required arguments for login
-	data = {
-		reqwaygguid: "",    // Don't know
-		evgguid: "",        // Don't know
-		size: 1024,         // Don't know
-		u: query.user,      // Username
-		p: query.password,  // Password
-		login: "> Login",   // Don't know
-		Newsletter: ""      // Don't know
-	};
+  // Required arguments for login
+  var data = {
+    u: query.user,      // Username
+    p: query.password,  // Password
+  };
 
-	needle.post("https://www.campus.rwth-aachen.de/office/views/campus/redirect.asp", data, {}, function (error, response) {
+  needle.post("https://www.campus.rwth-aachen.de/office/views/campus/redirect.asp", data, {}, function (error, response) {
 
-		var cookies = response.headers['set-cookie'];
-		cookies.forEach(function (item) {
+    var cookies = response.headers['set-cookie'];
+    cookies.forEach(function (item) {
 
-			// Because the server send some metadata we only need the first part of each Cookie.
-			loginCookies.push(item.split(";")[0]);
+      // Because the server send some metadata we only need the first part of each Cookie.
+      loginCookies.push(item.split(";")[0]);
 
-		});
+    });
 
-		var options = {headers: {Cookie: loginCookies.join(";")}}; // Add the cookies to the header
+    var options = {headers: {Cookie: loginCookies.join(";")}}; // Add the cookies to the header
 
-		needle.get("https://www.campus.rwth-aachen.de/office/views/calendar/iCalExport.asp?startdt=01.10.2014&enddt=31.03.2015%2023:59:59", options, function (error, response) {
-			// The Unicode converting is probably a dirty fix but it works. Otherwise some chars won't be displayed correctly.
-			res.write(decode_utf8(response.body));
-			res.end();
-		});
+    var start = moment().subtract(1, 'Month').format('DD.MM.YYYY');
+    var end   = moment().add(8, 'Month').format('DD.MM.YYYY');
 
-	});
+    needle.get('https://www.campus.rwth-aachen.de/office/views/calendar/iCalExport.asp?startdt='+start+'&enddt='+end+'%2023:59:59', options, function (error, response) {
+      res.header('Content-Type', 'text/calendar; charset=UTF-8'); // Set correct Content-Type
+      res.header('X-PUBLISHED-TTL', 'PT1H'); // Set update interval to 1h (Exchange MS)
+      res.header('REFRESH-INTERVAL', 'PT1H'); // Offical Spec
+      res.write(response.body);
+      res.end();
+    });
+
+  });
 });
 
 server.listen(2014);
+
 console.log("Server is listening");
 
-function decode_utf8(s) {
-	return decodeURIComponent(escape(s));
-}
